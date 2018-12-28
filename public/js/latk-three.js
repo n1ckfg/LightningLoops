12ad248975af88cdf628cddfa05a4e6b9574b368
Nick Fox-Gieg <nick@fox-gieg.com>
2018-12-27 22:16:57 -0600

    zipfile internal json can have any name

--- public/js/latk-three.js
+++ public/js/latk-three.js
@@ -752,9 +752,16 @@ function latkStart() {
 
             var zip = new JSZip();
             zip.loadAsync(data).then(function () {
-                var fileNameOrig = animationPath.split('\\').pop().split('/').pop();
-                var fileName = fileNameOrig.split('.')[0] + ".json";
-                zip.file(fileName).async("string").then(function(response) {
+                //var fileNameOrig = animationPath.split('\\').pop().split('/').pop();
+                //var fileName = fileNameOrig.split('.')[0] + ".json";
+                //zip.file(0).async("string").then(function(response) {
+
+                // https://github.com/Stuk/jszip/issues/375
+                var entries = Object.keys(zip.files).map(function (name) {
+                  return zip.files[name];
+                });
+           
+                zip.file(entries[0].name).async("string").then(function(response) {
                     jsonToGp(JSON.parse(response).grease_pencil[0]);
                 });
             });

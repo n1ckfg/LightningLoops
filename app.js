add17a80f6a80c590ae27c33ef27102b22706e92
Nick Fox-Gieg <nick@fox-gieg.com>
2019-01-19 22:00:20 -0500

    new test

--- app.js
+++ app.js
@@ -1,114 +1,46 @@
-"use strict";
+'use strict';
 
-var strokeLifetime = 10000;
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5555;
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const server = app.listen(PORT);
+const chalk = require('chalk');
+const socket = require('socket.io').listen(server);
+// socket.set('log level', 0);
+const ejs = require('ejs');
 
-var express = require("express");
-var app = express();
-var http = require("http").Server(app);
-var port = 8080;
+console.log(chalk.underline.green('Server is running on port ' + PORT));
 
-var io = require("socket.io")(http, { 
-	// default -- pingInterval: 1000 * 25, pingTimeout: 1000 * 60
-	// low latency -- pingInterval: 1000 * 5, pingTimeout: 1000 * 10
+app.set('views', __dirname + '/views');
+app.engine('.html', ejs.__express);
+app.set('view-engine', 'html');
+app.use(express.static(__dirname + '/public'));
 
-	pingInterval: 1000 * 25,
-	pingTimeout: 1000 * 60
+let isConnected = false;
+let isStreaming = false;
+let currentTimeSinceStreamStarted = 0.0;
+let currentFrameSinceStreamStarted = 0;
+let userCounter = 0;
+
+app.get('/', (req, res)=>{
+    // res.render('debug.html');
 });
 
-// ~ ~ ~ ~
-	
-app.use(express.static("public")); 
-
-app.get("/", function(req, res) {
-	res.sendFile(__dirname + "/public/index.html");
-});
-
-http.listen(port, function() {
-	console.log("\nNode app started. Listening on port " + port);
-});
-
-// ~ ~ ~ ~
-
-class Frame {
-	constructor() {
-		this.strokes = [];
-	}
-}
-
-class Layer {
-    constructor() {
-        this.frames = [];
+socket.on('connection', client => {
+    
+    userCounter++;
+    if (process.env.LOGGING_VERBOSE) {
+        console.log(chalk.bold.white('A user connected, we currently have ' + userCounter + ' users connected'));
     }
-
-    getFrame(index) {
-    	if (!this.frames[index]) {
-    		//console.log("Client asked for frame " + index +", but it's missing.");
-    		for (var i=0; i<index+1; i++) {
-    			if (!this.frames[i]) {
-    				var frame = new Frame();
-    				this.frames.push(frame); 
-    				//console.log("* Created frame " + i + ".");
-    			}
-    		}
-    	}
-        //console.log("Retrieving frame " + index + " of " + this.frames.length + ".");
-        return this.frames[index];
-    }
-
-    addStroke(data) {
-        try {
-    	var index = data["index"];
-    	if (!isNaN(index)) {
-    		this.getFrame(index); 
-    		this.frames[index].strokes.push(data); 
-            console.log("<<< Received a stroke with color (" + data["color"] + ") and " + data["points"].length + " points.");
-    	}
-        } catch (e) {
-            console.log(e.data);
+    
+    client.emit('variable-name', 'value here');
+    
+    client.on('disconnect', ()=>{
+        if (process.env.LOGGING_VERBOSE) {
+            console.log(chalk.bold.white('A user disconnected, we currently have ' + userCounter + ' users connected'))
         }
-    }
-}
-
-var layer = new Layer();
-
-setInterval(function() {
-	var time = new Date().getTime();
-
-	for (var i=0; i<layer.frames.length; i++) {
-		for (var j=0; j<layer.frames[i].strokes.length; j++) {
-			if (time > layer.frames[i].strokes[j]["timestamp"] + strokeLifetime) {
-				layer.frames[i].strokes.splice(j, 1);
-				console.log("X Removing frame " + i + ", stroke " + j + ".");
-			}
-		}
-	}
-}, strokeLifetime);
-
-// ~ ~ ~ ~
-
-// https://socket.io/get-started/chat/
-
-io.on('connection', function(socket){
-    console.log('A user connected.');
-    //~
-    socket.on('disconnect', function(){
-        console.log('A user disconnected.');
     });
-    //~
-    socket.on("clientStrokeToServer", function(data) { 
-    	//console.log(data);
-    	layer.addStroke(data);
-    });
-    //~
-    socket.on("clientRequestFrame", function(data) {
-        //console.log(data["num"]);
-        var index = data["num"];
-        if (index != NaN) {
-        	var frame = layer.getFrame(index);
-        	if (frame && frame.strokes.length > 0) {
-        		io.emit("newFrameFromServer", frame.strokes);
-                console.log("> > > Sending a new frame " + frame.strokes[0]["index"] + " with " + frame.strokes.length + " strokes.");
-        	}
-    	}
-    });
+
 });

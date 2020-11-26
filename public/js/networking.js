b77fec3ab3ddc67bc461b16c931af2dd2e669872
Nick Fox-Gieg <nick@fox-gieg.com>
2020-11-26 17:49:56 -0500

    testing

--- public/js/networking.js
+++ public/js/networking.js
@@ -1,19 +1,19 @@
 "use strict";
 
-var socket = io();
+let socket = io();
 
 socket.on("newFrameFromServer", function(data) {
 	if (latkDebug) console.log("Receiving new frame " + data[0]["index"] + " with " + data.length + " strokes.");
-    var newStrokes = [];
+    let newStrokes = [];
 
-    for (var i=0; i<data.length; i++) {
-        var geometry = new THREE.Geometry();
+    for (let i=0; i<data.length; i++) {
+        let geometry = new THREE.Geometry();
         geometry.dynamic = true;
 
-        var origVerts = [];
+        let origVerts = [];
 
-        for (var j=0; j<data[i]["points"].length; j++) {
-        	var co = data[i]["points"][j]["co"];
+        for (let j=0; j<data[i]["points"].length; j++) {
+        	let co = data[i]["points"][j]["co"];
             origVerts.push(new THREE.Vector3(co[0], co[1], co[2]));
 
             //if (j === 0 || !useMinDistance || (useMinDistance && origVerts[j].distanceTo(origVerts[j-1]) > minDistance)) {
@@ -27,13 +27,13 @@ socket.on("newFrameFromServer", function(data) {
 
         geometry.verticesNeedUpdate = true;
 
-        var line = new THREE.MeshLine();
+        let line = new THREE.MeshLine();
         line.setGeometry(geometry);
-        var meshLine = new THREE.Mesh(line.geometry, createUniqueMtl(serverColor));
+        let meshLine = new THREE.Mesh(line.geometry, createUniqueMtl(serverColor));
         newStrokes.push(meshLine);//line);
 	}
 
-    var index = data[0]["index"];
-    var last = layers.length - 1;
+    let index = data[0]["index"];
+    let last = layers.length - 1;
   	if (newStrokes.length > 0 && layers.length > 0 && layers[last].frames) layers[last].frames[index] = newStrokes;
 });
